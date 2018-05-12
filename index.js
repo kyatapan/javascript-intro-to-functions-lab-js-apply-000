21160b1ef245377f0b00cdf2aba7440262466b20
kathrin v <go.kathrin@gmail.com>
2018-05-12 00:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function sayHiToGrandma(string) {
   if(string == 'I love you, Grandma.') {
     return 'I love you, too.';
   }
-  else if (string === uppercase) {
+  else if (string == uppercase) {
     return 'YES INDEED!';
   }
 }

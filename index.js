ce2689f42d23eb6f1da28c694457bc24a8609356
kathrin v <go.kathrin@gmail.com>
2018-05-12 01:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(`${string.toUpperCase}`);
+}
 
-
+function logwhisper(string) {
+  console.log(`${string.toLowerCase}`);
+}
 
 function sayHiToGrandma(string) {
   
